29adeb44aca3ff6645e419fac437bd3464d86405
brian-loric <brian@loricgames.com>
2025-09-28 02:38:15 -0400

    maybe

--- dist/Workspace/conanbuild.bat
+++ dist/Workspace/conanbuild.bat
@@ -1,2 +1,2 @@
 @echo off
-call "%~dp0/conanbuildenv-release-x86_64.bat"
+call "%~dp0/conanbuildenv-debug-x86_64.bat"
--- dist/Workspace/deactivate_conanbuild.bat
+++ dist/Workspace/deactivate_conanbuild.bat
@@ -1,2 +1,2 @@
 @echo off
-call "%~dp0\deactivate_conanbuildenv-release-x86_64.bat"
+call "%~dp0\deactivate_conanbuildenv-debug-x86_64.bat"
